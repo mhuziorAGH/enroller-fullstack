38a9403134b020a0b9a057c5917fcb3c23c90c9b
Student <student@d17-426a-pc-13.ipa.ii.agh.edu.pl>
2026-05-17 14:23:46 +0200

    Dodana funkcja Delete ale z błędem nowegeo spatkana

--- src/main/frontend/src/meetings/MeetingsList.js
+++ src/main/frontend/src/meetings/MeetingsList.js
@@ -1,10 +1,13 @@
-export default function MeetingsList({meetings}) {
+import './MeetingsList.css';
+
+export default function MeetingsList({meetings, onDelete}) {
     return (
         <table>
             <thead>
             <tr>
                 <th>Nazwa spotkania</th>
                 <th>Opis</th>
+                <th>Akcje</th>
             </tr>
             </thead>
             <tbody>
@@ -12,6 +15,9 @@ export default function MeetingsList({meetings}) {
                 meetings.map((meeting, index) => <tr key={index}>
                     <td>{meeting.title}</td>
                     <td>{meeting.description}</td>
+                    <td>
+                        <button type="button" className="button-red button-outline" onClick={() => onDelete(meeting)}>Usuń</button>
+                    </td>
                 </tr>)
             }
             </tbody>
